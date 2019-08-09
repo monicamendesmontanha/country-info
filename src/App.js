39f20e57f7ba2cdcba9612708539a97edd639004
Monica Mendes <monicamendesmontanha@gmail.com>
2019-08-09 13:06:32 +1000

    Add form to search for a country and add style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <header className="header">
-        <h1>Country Information</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSearch(event) {
+    console.log("Searching for: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <h1>Country Information</h1>
+        </header>
+
+        <form className="search-box" onSubmit={this.handleSearch}>
+          <label className="label-search-box">Country name:</label>
+          <input
+            type="text"
+            className="input-search-box"
+            name="countryName"
+            placeholder="Australia"
+            value={this.state.value}
+            onChange={this.handleInputChange}
+          />
+          <input type="submit" value="Search" className="button-search-box" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
