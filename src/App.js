3bc2b7c577e180c09d5d2c58b2cc4313a334c9ff
Monica Mendes <monicamendesmontanha@gmail.com>
2019-08-11 18:28:14 +1000

    Move state from App.js to SearchBox

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ class App extends React.Component {
     super(props);
     this.state = {
       selectedCountry: undefined,
-      countryName: "",
-      countries: [],
       countryVisible: false
     };
   }
@@ -17,33 +15,6 @@ class App extends React.Component {
   handleSearch = event => {
     event.preventDefault();
     this.setState({ countryVisible: true });
-  };
-
-  // called every time the input value changes
-  handleInputChange = (event, { newValue }) => {
-    this.setState({
-      countryName: newValue
-    });
-  };
-
-  // Autosuggest will call this function every time needed to update suggestions.
-  onSuggestionsFetchRequested = ({ value }) => {
-    const self = this;
-
-    if (value.length < 3) {
-      return;
-    }
-
-    fetch(`https://restcountries.eu/rest/v2/name/${value}`)
-      .then(response => response.json())
-      .then(countries => {
-        self.setState({ countries });
-      });
-  };
-
-  // Autosuggest will call this function every time needed to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({ countries: [] });
   };
 
   // Will be called every time suggestion is selected via mouse or keyboard.
@@ -59,12 +30,7 @@ class App extends React.Component {
         </header>
         <SearchBox
           handleSearch={this.handleSearch}
-          countries={this.state.countries}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
           onSuggestionSelected={this.onSuggestionSelected}
-          countryName={this.state.countryName}
-          handleInputChange={this.handleInputChange}
         />
         {this.state.selectedCountry && this.state.countryVisible ? (
           <CountryInformation country={this.state.selectedCountry} />
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,41 +1,73 @@
 import React from "react";
 import Autosuggest from "react-autosuggest";
 
-const SearchBox = ({
-  handleSearch,
-  countries,
-  onSuggestionsFetchRequested,
-  onSuggestionsClearRequested,
-  onSuggestionSelected,
-  countryName,
-  handleInputChange
-}) => {
-  return (
-    <form className="search-box" onSubmit={handleSearch}>
-      <label className="label-search-box">Country name:</label>
-      <Autosuggest
-        className="input-search-box"
-        suggestions={countries}
-        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={onSuggestionsClearRequested}
-        onSuggestionSelected={onSuggestionSelected}
-        getSuggestionValue={suggestion => suggestion.name}
-        renderSuggestion={country => (
-          <div>
-            {country.name} - {country.alpha3Code}
-          </div>
-        )}
-        inputProps={{
-          placeholder: "Type a country name",
-          value: countryName,
-          onChange: handleInputChange
-        }}
-      />
-      <button type="submit" className="button-search-box">
-        Search
-      </button>
-    </form>
-  );
-};
+class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      countryName: "",
+      countries: []
+    };
+  }
+
+  // called every time the input value changes
+  handleInputChange = (event, { newValue }) => {
+    this.setState({
+      countryName: newValue
+    });
+  };
+
+  // Autosuggest will call this function every time needed to update suggestions.
+  onSuggestionsFetchRequested = ({ value }) => {
+    const self = this;
+
+    if (value.length < 3) {
+      return;
+    }
+
+    fetch(`https://restcountries.eu/rest/v2/name/${value}`)
+      .then(response => response.json())
+      .then(countries => {
+        self.setState({ countries });
+      });
+  };
+
+  // Autosuggest will call this function every time needed to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({ countries: [] });
+  };
+
+  render() {
+    const { handleSearch, onSuggestionSelected } = this.props;
+
+    return (
+      <form className="search-box" onSubmit={handleSearch}>
+        <label className="label-search-box">Country name:</label>
+        <Autosuggest
+          className="input-search-box"
+          suggestions={this.state.countries}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          onSuggestionSelected={onSuggestionSelected}
+          getSuggestionValue={suggestion => suggestion.name}
+          renderSuggestion={country => (
+            <div>
+              {country.name} - {country.alpha3Code}
+            </div>
+          )}
+          inputProps={{
+            placeholder: "Type a country name",
+            value: this.state.countryName,
+            onChange: this.handleInputChange
+          }}
+        />
+        <button type="submit" className="button-search-box">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
 
 export default SearchBox;
