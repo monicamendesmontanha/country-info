4b195f017f8c938ce785c349d67af362cb88820e
Monica Mendes <monicamendesmontanha@gmail.com>
2019-08-11 19:52:24 +1000

    Show the history of searches (recent first)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends React.Component {
     super(props);
     this.state = {
       selectedCountry: undefined,
-      countryVisible: false
+      countryVisible: false,
+      searchHistory: [],
+      countries: []
     };
   }
 
@@ -17,9 +19,86 @@ class App extends React.Component {
     this.setState({ countryVisible: true });
   };
 
+  // Autosuggest will call this function every time needed to update suggestions.
+  onSuggestionsFetchRequested = ({ value }) => {
+    const self = this;
+
+    if (value.length < 3) {
+      return;
+    }
+
+    fetch(`https://restcountries.eu/rest/v2/name/${value}`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.status && jsonResponse.status === 404) {
+          self.setState({ countries: [] });
+        } else {
+          // get top 10
+          const top10 = jsonResponse.slice(0, 9);
+
+          // get search history from the state
+          const searchHistory = this.state.searchHistory;
+
+          // type "can"
+          // history: []
+          // top10: ["Canada", "Central African Republic"]
+          // select: "Canada"
+          // history: ["Canada"]
+
+          // type "bra"
+          // top10: ["Brazil", "Gibraltar"]
+          // history: ["Canada"] - No show!
+          // select: "Brazil"
+          // history: ["Canada", "Brazil"]
+
+
+         const top10Names = top10.map(country => country.name);
+
+         // remove countries from the search history that doesn't match with the search
+         const searchHistoryWithMatchingResultsOnly = searchHistory.filter(country => {
+            return top10Names.includes(country.name);
+         });
+
+          // eliminate duplicates from the search history
+          const searchHistoryNames = searchHistoryWithMatchingResultsOnly.map(item => item.name);
+          const countriesThatAreNotInSearchHistory = top10.filter(country => {
+            return !searchHistoryNames.includes(country.name);
+          });
+
+          // combine search history with the top10
+          const searchResult = searchHistoryWithMatchingResultsOnly.concat(
+            countriesThatAreNotInSearchHistory
+          );
+
+          // set the state with the search result
+          self.setState({ countries: searchResult });
+        }
+      });
+  };
+
   // Will be called every time suggestion is selected via mouse or keyboard.
   onSuggestionSelected = (event, { suggestion }) => {
-    this.setState({ selectedCountry: suggestion });
+    this.setState({
+      selectedCountry: suggestion,
+    });
+
+    const searchHistory = this.state.searchHistory;
+    const searchHistoryNames = searchHistory.map(country => country.name);
+
+    // check if suggestion is already included on the search history
+    if (!searchHistoryNames.includes(suggestion.name)) {
+      // add suggestion to the end of the search history
+      const searchHistoryWithNewSuggestion = searchHistory.concat(suggestion);
+
+      this.setState({
+        searchHistory: searchHistoryWithNewSuggestion.reverse()
+      });
+    }
+  };
+
+  // Autosuggest will call this function every time needed to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({ countries: [] });
   };
 
   render() {
@@ -29,7 +108,10 @@ class App extends React.Component {
           <h1>Country Information</h1>
         </header>
         <SearchBox
+          countries={this.state.countries}
           handleSearch={this.handleSearch}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
           onSuggestionSelected={this.onSuggestionSelected}
         />
         {this.state.selectedCountry && this.state.countryVisible ? (
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,8 +6,7 @@ class SearchBox extends React.Component {
     super(props);
 
     this.state = {
-      countryName: "",
-      countries: []
+      countryName: ""
     };
   }
 
@@ -18,42 +17,23 @@ class SearchBox extends React.Component {
     });
   };
 
-  // Autosuggest will call this function every time needed to update suggestions.
-  onSuggestionsFetchRequested = ({ value }) => {
-    const self = this;
-
-    if (value.length < 3) {
-      return;
-    }
-
-    fetch(`https://restcountries.eu/rest/v2/name/${value}`)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        if (jsonResponse.status && jsonResponse.status === 404) {
-          self.setState({ countries: [] });
-        } else {
-          const top10 = jsonResponse.slice(0, 9);
-          self.setState({ countries: top10 });
-        }
-      });
-  };
-
-  // Autosuggest will call this function every time needed to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({ countries: [] });
-  };
-
   render() {
-    const { handleSearch, onSuggestionSelected } = this.props;
+    const {
+      countries,
+      handleSearch,
+      onSuggestionsClearRequested,
+      onSuggestionsFetchRequested,
+      onSuggestionSelected
+    } = this.props;
 
     return (
       <form className="search-box" onSubmit={handleSearch}>
         <label className="label-search-box">Country name:</label>
         <Autosuggest
           className="input-search-box"
-          suggestions={this.state.countries}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          suggestions={countries}
+          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={onSuggestionsClearRequested}
           onSuggestionSelected={onSuggestionSelected}
           getSuggestionValue={suggestion => suggestion.name}
           renderSuggestion={country => (
